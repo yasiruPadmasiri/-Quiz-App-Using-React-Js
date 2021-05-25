a09ea7bd9829579e9888a9cf991bec68e55b61fc
yasiruPadmasiri <66236929+yasiruPadmasiri@users.noreply.github.com>
2021-05-25 13:42:54 +0530

    changing functions and awsers

--- quiz-app/src/component/Quiz.jsx
+++ quiz-app/src/component/Quiz.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+import { useState } from 'react';
 import QuizData from '../QuizData'
 
 
@@ -7,52 +6,46 @@ import QuizData from '../QuizData'
 var  quizNumber=0;
 var score=0;
 
-// const [quizNumber,setQuizNmber]=useState(0);
-
-var answer=QuizData[quizNumber].answer;
-var answers=QuizData[quizNumber].options
-
-
-function nextquizGo(){
-    quizNumber++;
-    console.log("quiz number is "+quizNumber);
-   LoadQuiz();
- 
-}
-function selectAnswer(){
-
-}
-
-function AnswersList(answers){
-    console.log(quizNumber);
-    console.log(answers)
-    
-    return(
-        <div>
-            <ul>
-                <li onClick={selectAnswer}><button>{answers}</button></li>
-            </ul>
-        </div>
-    )
-}
 
  function LoadQuiz(){
-     var x=0;
-     x++;
-     console.log(x);
-     console.log(QuizData[quizNumber].question)
-     console.log(answer);
-     console.log(QuizData[quizNumber].options)
-     return(<div>
+    const [questionIndex, setQuestionIndex] = useState(0);
+
+    function nextquizGo(){
+        if(questionIndex < 6){
+            quizNumber++;
+            setQuestionIndex(quizNumber);
+        }else if(questionIndex>=6){
+           
+        }
+
+        console.log("quiz number is "+quizNumber);
+     
+    }
+    function selectAnswer(){
+    
+    }
+    function AnswersList(answer,index){
+        return <li key={index}>{answer}</li>
+    }
+
+
+     return(
+     <div>
          <div className="col-md5">
-            <button readonly>Score : {score}</button>
+            <button >Score : {score}</button>
          </div>
-         <h2>{QuizData[quizNumber].question}</h2>
-   {answers.map(AnswersList)}
-   <div>
-       <button className="btn btn-success" onClick={nextquizGo}>Next Question</button>
-       
-   </div>
+
+         <h2>{QuizData[questionIndex].question}</h2>
+
+         {questionIndex < QuizData.length &&
+            <div>
+                {QuizData[questionIndex].options.map(AnswersList)}
+            </div>
+         }
+
+        <div>
+            <button className="btn btn-success" onClick={nextquizGo}>Next Question</button>
+        </div>
          
      </div>)
  }

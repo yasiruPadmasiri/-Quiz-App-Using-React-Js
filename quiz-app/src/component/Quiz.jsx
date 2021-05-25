75996a666e293131d01bfc10b730ce2fd6cba581
yasiruPadmasiri <66236929+yasiruPadmasiri@users.noreply.github.com>
2021-05-25 22:06:53 +0530

    score increments working

--- quiz-app/src/component/Quiz.jsx
+++ quiz-app/src/component/Quiz.jsx
@@ -5,17 +5,37 @@ import QuizData from '../QuizData'
 
 var  quizNumber=0;
 var score=0;
+var isSelect=false
 
 
  function LoadQuiz(){
     const [questionIndex, setQuestionIndex] = useState(0);
+    const [userAnswer,setUserAnswer]=useState(" ");
+    const [buttonColor,setCoulor]=useState(
+        {backgroundColor:"green",
+        margin:10,
+        textAlign:'center'            
+        })
+        const [score,setScore]=useState(0)
+
+
+    function perviousquiz(){
+        if(questionIndex>0){
+            quizNumber--;
+            setQuestionIndex(quizNumber);
+        }
+    }
 
     function nextquizGo(){
+        var newScore=0;
         if(questionIndex < 6){
             quizNumber++;
             setQuestionIndex(quizNumber);
-        }else if(questionIndex>=6){
-           
+            console.log(questionIndex);
+            if(userAnswer==QuizData[questionIndex].answer){
+                newScore=score+10;
+                setScore(newScore);
+            }
         }
 
         console.log("quiz number is "+quizNumber);
@@ -24,10 +44,29 @@ var score=0;
     function selectAnswer(){
     
     }
+
+    
+
     function AnswersList(answer,index){
-        return <li key={index}>{answer}</li>
+
+        function changecolor(){
+            console.log("clicked answer si "+answer)
+            setUserAnswer(answer);
+            setCoulor({backgroundColor:"white"})
+            console.log("user answer is " +QuizData[questionIndex].answer);
+        
+
+        }
+
+        return (
+            <div key={index}>
+        <button onClick={changecolor} style={buttonColor} key={index}>{answer}</button>
+        </div>
+       
+        )
     }
 
+    
 
      return(
      <div>
@@ -42,9 +81,12 @@ var score=0;
                 {QuizData[questionIndex].options.map(AnswersList)}
             </div>
          }
+         
+                  
 
         <div>
-            <button className="btn btn-success" onClick={nextquizGo}>Next Question</button>
+            <button className="btn btn-success" style={{margin:20}} onClick={nextquizGo}>Next Question</button>
+            <button className="btn btn-danger" style={{margin:50}} onClick={perviousquiz}>Previous Question</button>
         </div>
          
      </div>)
